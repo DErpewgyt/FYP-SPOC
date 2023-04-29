6c3360f1f18b7365966661161e6deff774615166
isaiah-st <109123090+isaiah-st@users.noreply.github.com>
2023-04-29 17:47:34 +0800

    working on clicking keratometer components to go to keratometer viewport #1 (continued)

--- Assets/FYP Project/Scripts/ObjectClicker.cs
+++ Assets/FYP Project/Scripts/ObjectClicker.cs
@@ -3,7 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectClicker : MonoBehaviour
+
+
 {
+    public GameObject parent;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = parent.GetComponentInParent<Animator>();
+    }
+
     // Update is called once per frame
     private void Update()
     {
@@ -17,6 +27,7 @@ public class ObjectClicker : MonoBehaviour
                 if (hit.transform != null)
                 {
                     PrintName(hit.transform.gameObject);
+                    anim.Play("keratometerviewer");
                 }
             }
         }
@@ -24,11 +35,52 @@ public class ObjectClicker : MonoBehaviour
 
     private void PrintName(GameObject go)
     {
-        print(go.name); 
-    }
-
-    private void LeftKnob()
-    {
-
+        print(go.name);
+        anim.Play("keratometerviewer");
     }
 }
+
+
+
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClicker : MonoBehaviour
+
+
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    PrintName(hit.transform.gameObject);
+                    anim.Play("keratometerzoom");
+                }
+            }
+        }
+    }
+
+    private void PrintName(GameObject go)
+    {
+        print(go.name);
+        anim.Play("keratometerzoom");
+    }
+}
+*/
