77de6155642347334297bce7f868302496ef647a
Jeronlow <65123862+Jeronlow@users.noreply.github.com>
2023-05-09 20:57:23 +0800

    Added limit for keretometer scroll (left, top and blurry circle) & updated blurry circle #20

--- Assets/FYP Project/Scripts/MoveTest.cs
+++ Assets/FYP Project/Scripts/MoveTest.cs
@@ -9,15 +9,23 @@ public class MoveTest : MonoBehaviour
     public bool HorizontalCircle;
     public bool CircleMovement;
 
+    public GameObject parentObject;
     public GameObject BlurryCircle;
+    public GameObject MiddleCircle;
     public GameObject LeftCircle;
     public GameObject TopCircle;
 
     public float maxDistX = .3f;
     public float maxDistZ = .1f;
     public float maxDistY = .1f;
+
+    public float maxValue = .2f;
+    public float minValue = .025f;
+    public float maxDistZScroll = .1f;
     // Define the starting position variable as a static variable
     private static Vector3 startingPos;
+    private static Vector3 BlurStartingPos;
+
 
     // Define the speed and maximum distance variables
     public float speed = 5f;
@@ -25,13 +33,14 @@ public class MoveTest : MonoBehaviour
     private void Start()
     {
         startingPos = transform.position;
-        maxDistZ = Mathf.Abs(startingPos.z);
+        BlurStartingPos = BlurryCircle.transform.position;
     }
 
     private void Update()
     {
         if (CircleMovement)
         {
+
             Vector3 moveDir = new Vector3(0, 0, 0);
 
             if (Input.GetKey(KeyCode.A))
@@ -46,16 +55,28 @@ public class MoveTest : MonoBehaviour
                 print(moveDir.x);
             }
 
-            if (Input.GetKey(KeyCode.S))
+            if (Input.GetKey(KeyCode.Q))
             {
                 moveDir.y = -1f;
                 print(moveDir.y);
             }
 
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.E))
             {
                 moveDir.y = 1f;
                 print(moveDir.y);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveDir.z = -1f;
+                print(moveDir.z);
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveDir.z = 1f;
+                print(moveDir.z);
             }
 
             // Check if any of the movement keys are pressed
@@ -72,7 +93,8 @@ public class MoveTest : MonoBehaviour
             }
         }
 
-        if (HorizontalCircle)
+        /**
+                 if (HorizontalCircle)
         {
             float xDirection = Input.GetAxisRaw("Mouse ScrollWheel");
 
@@ -95,9 +117,7 @@ public class MoveTest : MonoBehaviour
 
                 TopCircle.transform.position += moveDirectionY * speed;
             }
-        }
-
-        else if (BlurCircle)
+        }        else if (BlurCircle)
         {
             float zDirection = Input.GetAxis("Mouse ScrollWheel");
 
@@ -105,5 +125,87 @@ public class MoveTest : MonoBehaviour
 
             BlurryCircle.transform.position += moveDirectionZ * speed;
         }
+        **/
+
+
+
+        float xDirection = Input.GetAxisRaw("Mouse ScrollWheel");
+        if (HorizontalCircle && Mathf.Abs(xDirection) > 0.05f)
+        {
+            Vector3 moveDirectionX = new Vector3(xDirection, 0.0f, 0.0f);
+
+            // Calculate the target position
+            Vector3 targetPosx = LeftCircle.transform.position + moveDirectionX * speed;
+
+            // Clamp the target position within the maximum distance
+            targetPosx.x = Mathf.Clamp(targetPosx.x, MiddleCircle.transform.position.x - maxValue, MiddleCircle.transform.position.x - minValue);
+
+            // Move the circle to the target position
+            LeftCircle.transform.position = targetPosx;
+        }
+
+        float yDirection = Input.GetAxisRaw("Mouse ScrollWheel");
+        if (VerticalCircle && Mathf.Abs(yDirection) > 0.05f)
+        {
+            Vector3 moveDirectionY = new Vector3(0.0f, yDirection, 0.0f);
+
+            // Calculate the target position
+            Vector3 targetPosy = TopCircle.transform.position + moveDirectionY * speed;
+
+            // Clamp the target position within the maximum distance
+            targetPosy.y = Mathf.Clamp(targetPosy.y, MiddleCircle.transform.position.y + minValue, MiddleCircle.transform.position.y + maxValue);
+
+            // Move the circle to the target position
+            TopCircle.transform.position = targetPosy;
+        }
+
+        float zDirection = Input.GetAxis("Mouse ScrollWheel");
+        if (BlurCircle && Mathf.Abs(zDirection) > 0.05f)
+        {
+            Vector3 moveDirectionZ = new Vector3(0.0f, 0.0f, zDirection);
+
+            // Calculate the target position
+            Vector3 targetPosz = BlurryCircle.transform.position + moveDirectionZ * speed;
+
+            // Clamp the target position within the maximum distance
+            targetPosz.z = Mathf.Clamp(targetPosz.z, BlurStartingPos.z - maxDistZScroll, BlurStartingPos.z + maxDistZScroll);
+
+            // Move the circle to the target position
+            BlurryCircle.transform.position = targetPosz;
+        }
+
+        /**
+         if (HorizontalCircle)
+        {
+            float xDirection = Input.GetAxisRaw("Mouse ScrollWheel");
+
+            // Only move the circle if the scroll wheel input exceeds a threshold value
+            if (Mathf.Abs(xDirection) > 0.05f)
+            {
+                Vector3 moveDirectionX = new Vector3(xDirection, 0.0f, 0.0f);
+
+                LeftCircle.transform.position += moveDirectionX * speed;
+            }
+        }
+        else if (VerticalCircle)
+        {
+            float yDirection = Input.GetAxisRaw("Mouse ScrollWheel");
+
+            // Only move the circle if the scroll wheel input exceeds a threshold value
+            if (Mathf.Abs(yDirection) > 0.05f)
+            {
+                Vector3 moveDirectionY = new Vector3(0.0f, yDirection, 0.0f);
+
+                TopCircle.transform.position += moveDirectionY * speed;
+            }
+        }        else if (BlurCircle)
+        {
+            float zDirection = Input.GetAxis("Mouse ScrollWheel");
+
+            Vector3 moveDirectionZ = new Vector3(0.0f, 0.0f, zDirection);
+
+            BlurryCircle.transform.position += moveDirectionZ * speed;
+        }
+        **/
     }
 }
