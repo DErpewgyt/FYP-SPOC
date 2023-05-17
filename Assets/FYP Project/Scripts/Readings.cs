6e73b6664c9b155f515f13678e15aa4d9d57c0b5
isaiah-st <109123090+isaiah-st@users.noreply.github.com>
2023-05-17 18:13:35 +0800

    Implemented different readings for keratometer #48 (continued)

--- Assets/FYP Project/Scripts/Readings.cs
+++ Assets/FYP Project/Scripts/Readings.cs
@@ -3,15 +3,15 @@ using TMPro;
 
 public class Readings : MonoBehaviour
 {
-    public TextMeshProUGUI text1;
-    public TextMeshProUGUI text2;
+    public TextMeshProUGUI reading1;
+    public TextMeshProUGUI reading2;
 
     void Start()
     {
-        float randomNum1 = Random.Range(5.5f, 12f);
-        float randomNum2 = Random.Range(12f, 28f);
+        float readingrandom1 = Random.Range(5.5f, 12f);
+        float readingrandom2 = Random.Range(12f, 28f);
 
-        text1.text = randomNum1.ToString("F2");
-        text2.text = randomNum2.ToString("F2");
+        reading1.text = readingrandom1.ToString("F2"); //2 dp
+        reading2.text = readingrandom2.ToString("F2");
     }
 }
