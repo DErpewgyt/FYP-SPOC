eba8e3ba669806cdf42fb6632cb4074833945769
Jayden Lim <jaydenlimxw24.21@ichat.sp.edu.sg>
2023-06-29 16:31:08 +0800

    Fixed astigmatism animation and pd scale #62

--- Assets/FYP Project/Scripts/Phoropter/PDController.cs
+++ Assets/FYP Project/Scripts/Phoropter/PDController.cs
@@ -18,30 +18,26 @@ public class PDController : MonoBehaviour
 
     private void Start()
     {
-        scrollLimit = 0;
+        /*scrollLimit = 0;*/
     }
     public void Update()
     {
         float scrollInput = Input.GetAxis("Mouse ScrollWheel");
 
-        if (scrollInput > 0 && scrollLimit < 21)
+        if (scrollInput > 0 && pd < scrollLimit)
         {
             // Scroll outwards
             leftHalf.transform.position -= scrollDirection;
             rightHalf.transform.position += scrollDirection;
-            scrollLimit += 1;
             pd += 1;
-            pd = Mathf.Clamp(pd, 0, 20);
             //UpdateTmp();
         }
-        else if (scrollInput < 0 && scrollLimit > 0)
+        else if (scrollInput < 0 && pd > 0)
         {
             // Scroll inwards
             leftHalf.transform.position += scrollDirection;
             rightHalf.transform.position -= scrollDirection;
-            scrollLimit -= 1;
             pd -= 1;
-            pd = Mathf.Clamp(pd, 0, 20);
            //UpdateTmp();
         }
     }
