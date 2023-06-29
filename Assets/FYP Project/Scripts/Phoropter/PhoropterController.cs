2898422d1c204ff0bca8d287af8da6b754e15575
Jeronlow <65123862+Jeronlow@users.noreply.github.com>
2023-06-29 18:58:30 +0800

    Implement phoropter astigmatism magnitude degree #85

--- Assets/FYP Project/Scripts/Phoropter/PhoropterController.cs
+++ Assets/FYP Project/Scripts/Phoropter/PhoropterController.cs
@@ -12,6 +12,7 @@ public class PhoropterController : MonoBehaviour
     public GameObject RulerController;
     public GameObject Ruler;
     public ShortLongSightMovement ShortLongSightScript;
+    public AstigmatismMagnitudeControl AstigmatismMagnitudeControl;
     private string[] allowedTags = { "PupillaryDistanceKnobLeft",
                                      "PupillaryDistanceKnobRight",
                                      "OpenAndCloseKnobLeft",
@@ -217,11 +218,13 @@ public class PhoropterController : MonoBehaviour
             // Handle functionality for AstigmatismMagnitudeKnobLeft
             case "AstigmatismMagnitudeKnobLeft":
                 print("AstigmatismMagnitudeKnobLeft clicked");
+                AstigmatismMagnitudeControl.AstigMagLeftBool = true;
                 break;
 
             // Handle functionality for AstigmatismMagnitudeKnobRight
             case "AstigmatismMagnitudeKnobRight":
                 print("AstigmatismMagnitudeKnobRight clicked");
+                AstigmatismMagnitudeControl.AstigMagRightBool = true;
                 break;
 
             // Handle functionality for AstigmatismAxisKnobLeft
@@ -246,5 +249,7 @@ public class PhoropterController : MonoBehaviour
         RulerController.SetActive(false);
         ShortLongSightScript.LeftLSSightBool = false;
         ShortLongSightScript.RightLSSightBool = false;
+        AstigmatismMagnitudeControl.AstigMagRightBool = false;
+        AstigmatismMagnitudeControl.AstigMagLeftBool = false;
     }
 }
