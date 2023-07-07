112860ce62fbfb0a0b73760eadadd2b26cc57197
Jeronlow <65123862+Jeronlow@users.noreply.github.com>
2023-07-07 21:35:19 +0800

    Minor fixes to AstigMagnitude (Function and animation)

--- Assets/FYP Project/Scripts/Phoropter/AstigmatismMagnitudeControl.cs
+++ Assets/FYP Project/Scripts/Phoropter/AstigmatismMagnitudeControl.cs
@@ -46,14 +46,15 @@ public class AstigmatismMagnitudeControl : MonoBehaviour
         {
             float rotationAmount = Input.mouseScrollDelta.y * rotationSpeed * Time.deltaTime;
 
-            RotateObject(LeftAstigmatismMagKnob, rotationAmount);
+
 
             TopLeftobject.SetActive(true);
             TopRightobject.SetActive(false);
             if (scrollInput != 0f)
             {
-                AstigMagLeft -= Mathf.Sign(scrollInput) * 0.25f;
-                AstigMagLeft = Mathf.Clamp(AstigMagLeft, 0.00f, 6.00f);
+                RotateObject(LeftAstigmatismMagKnob, rotationAmount, AstigMagLeft);
+                AstigMagLeft += Mathf.Sign(scrollInput) * -0.25f;
+                AstigMagLeft = Mathf.Clamp(AstigMagLeft, -6.00f, 0.00f);
                 UpdateValue(LeftValue, AstigMagLeft, TopLeftValue);
             }
         }
@@ -62,14 +63,13 @@ public class AstigmatismMagnitudeControl : MonoBehaviour
         {
             float rotationAmount = Input.mouseScrollDelta.y * rotationSpeed * Time.deltaTime;
 
-            RotateObject(RightAstigmatismMagKnob, rotationAmount);
-
             TopLeftobject.SetActive(false);
             TopRightobject.SetActive(true);
             if (scrollInput != 0f)
             {
-                AstigMagRight -= Mathf.Sign(scrollInput) * 0.25f;
-                AstigMagRight = Mathf.Clamp(AstigMagRight, 0.00f, 6.00f);
+                RotateObject(RightAstigmatismMagKnob, rotationAmount, AstigMagRight);
+                AstigMagRight += Mathf.Sign(scrollInput) * -0.25f;
+                AstigMagRight = Mathf.Clamp(AstigMagRight, -6.00f, 0.00f);
                 UpdateValue(RightValue, AstigMagRight, TopRightValue);
             }
         }
@@ -81,21 +81,26 @@ public class AstigmatismMagnitudeControl : MonoBehaviour
         }
     }
 
-    private void RotateObject(GameObject obj, float rotationAmount)
+    private void RotateObject(GameObject obj, float rotationAmount, float astigvalue)
     {
-        // Get the object's center position
-        Vector3 center = obj.GetComponent<Renderer>().bounds.center;
+        if (astigvalue < 0f && astigvalue > -6f)
+        {
+            // Get the object's center position
+            Vector3 center = obj.GetComponent<Renderer>().bounds.center;
 
-        // Calculate the rotation axis as the object's up vector
-        Vector3 rotationAxis = obj.transform.up;
+            // Calculate the rotation axis as the object's up vector
+            Vector3 rotationAxis = obj.transform.up;
 
-        // Rotate the object around its center for the Y-axis
-        obj.transform.RotateAround(center, rotationAxis, rotationAmount);
+            // Rotate the object around its center for the Y-axis
+            obj.transform.RotateAround(center, rotationAxis, rotationAmount);
+        }
+            return;
     }
 
     private void UpdateValue(TextMeshProUGUI valueText, float LSvalues, TextMeshProUGUI valueText2)
     {
-        string formattedValue = LSvalues.ToString("F2").Replace("0.", ".");
+        float displayValue = LSvalues * -1f;
+        string formattedValue = displayValue.ToString("F2").Replace("0.", ".");
         valueText.text = formattedValue;
         valueText.color = Color.red;
         valueText2.text = formattedValue;
