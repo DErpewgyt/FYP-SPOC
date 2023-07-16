1c78c1646a5ec81f35de63bfba8cb9157da70f51
Jeronlow <65123862+Jeronlow@users.noreply.github.com>
2023-07-17 00:59:55 +0800

    Implement objective indicator in phoropter scene #113

--- Assets/FYP Project/Scripts/Phoropter/ObjectiveScript.cs
+++ Assets/FYP Project/Scripts/Phoropter/ObjectiveScript.cs
@@ -22,35 +22,75 @@ public class ObjectiveScript : MonoBehaviour
 
     public int OBJIndicator1 = 1;
     public Animator LineObjective;
-    public TextMeshProUGUI AstigAxisTMPleft;
-    public RawImage AstigAxisLineleft;
-    public bool isAstigAxisAnimationPlayedleft = false;
+    public TextMeshProUGUI ObjectiveRefraction;
+    public RawImage ObjectiveRefractionLine;
+    public bool ObjectiveRefractionLineBool = false;
 
     public int OBJIndicator2 = 2;
     public Animator LineObjective2;
-    public TextMeshProUGUI AstigMagTMPleft;
-    public RawImage AstigMagLineleft;
-    public bool isAstigMagAnimationPlayedleft = false;
+    public TextMeshProUGUI CloseLeftEye;
+    public RawImage CloseLeftEyeLine;
+    public bool CloseLeftEyeLineBool = false;
     
     public int OBJIndicator3 = 3;
     public Animator LineObjective3;
+    public TextMeshProUGUI LSRight;
+    public RawImage LSRightLine;
+    public bool LSRightLineBool = false;
+
+    public int OBJIndicator4 = 4;
+    public Animator LineObjective4;
     public TextMeshProUGUI AstigAxisTMPright;
     public RawImage AstigAxisLineright;
     public bool isAstigAxisAnimationPlayedright = false;
 
-    public int OBJIndicator4 = 4;
-    public Animator LineObjective4;
+    public int OBJIndicator5 = 5;
+    public Animator LineObjective5;
     public TextMeshProUGUI AstigMagTMPright;
     public RawImage AstigMagLineright;
     public bool isAstigMagAnimationPlayedright = false;
-    
 
-    public int OBJIndicator5 = 5;
-    public Animator LineObjective5;
-    public TextMeshProUGUI PlaceHolderTMP;
-    public RawImage PlaceHolderIMG;
-    public bool PlaceHolderBool = false;
+    public int OBJIndicator6 = 6;
+    public Animator LineObjective6;
+    public TextMeshProUGUI RightEyeFinal;
+    public RawImage RightEyeFinalLine;
+    public bool RightEyeFinalLineBool = false;
 
+    public int OBJIndicator7 = 7;
+    public Animator LineObjective7;
+    public TextMeshProUGUI SwitchEyes;
+    public RawImage SwitchEyesLine;
+    public bool SwitchEyesLineBool = false;
+
+    public int OBJIndicator8 = 8;
+    public Animator LineObjective8;
+    public TextMeshProUGUI LSLeft;
+    public RawImage LSLeftLine;
+    public bool LSLeftLineBool = false;
+
+    public int OBJIndicator9 = 9;
+    public Animator LineObjective9;
+    public TextMeshProUGUI AstigAxisTMPleft;
+    public RawImage AstigAxisLineleft;
+    public bool isAstigAxisAnimationPlayedleft = false;
+
+    public int OBJIndicator10 = 10;
+    public Animator LineObjective10;
+    public TextMeshProUGUI AstigMagTMPleft;
+    public RawImage AstigMagLineleft;
+    public bool isAstigMagAnimationPlayedleft = false;
+
+    public int OBJIndicator11 = 11;
+    public Animator LineObjective11;
+    public TextMeshProUGUI LeftEyeFinal;
+    public RawImage LeftEyeFinalLine;
+    public bool LeftEyeFinalLineBool = false;
+
+    public int OBJIndicator12 = 12;
+    public Animator LineObjective12;
+    public TextMeshProUGUI Congrats;
+    public RawImage CongratsLine ;
+    public bool CongratsLineBool = false;
 
 
 
@@ -64,12 +104,32 @@ public class ObjectiveScript : MonoBehaviour
 
     public AIVoiceChecker Checker;
 
+    public GameObject Submit;
+
     void Start()
     {
-        MakeAlpha0(AstigMagTMPleft, AstigMagLineleft);
+        MakeAlpha0(CloseLeftEye, CloseLeftEyeLine);
+        MakeAlpha0(LSRight, LSRightLine);
         MakeAlpha0(AstigAxisTMPright, AstigAxisLineright);
         MakeAlpha0(AstigMagTMPright, AstigMagLineright);
-        MakeAlpha0(PlaceHolderTMP, PlaceHolderIMG);
+        MakeAlpha0(RightEyeFinal, RightEyeFinalLine);
+
+        MakeAlpha0(SwitchEyes, SwitchEyesLine);
+        MakeAlpha0(LSLeft, LSLeftLine);
+        MakeAlpha0(AstigAxisTMPleft, AstigAxisLineleft);
+        MakeAlpha0(AstigMagTMPleft, AstigMagLineleft);
+        MakeAlpha0(LeftEyeFinal, LeftEyeFinalLine);
+        MakeAlpha0(Congrats, CongratsLine);
+
+        Submit.SetActive(false);
+
+
+        //MakeAlpha0(AstigMagTMPleft, AstigMagLineleft);
+        //MakeAlpha0(AstigAxisTMPright, AstigAxisLineright);
+        //MakeAlpha0(AstigMagTMPright, AstigMagLineright);
+
+
+        //MakeAlpha0(PlaceHolderTMP, PlaceHolderIMG);
     }
 
     void MakeAlpha0(TextMeshProUGUI TMP, RawImage IMG)
@@ -81,25 +141,45 @@ public class ObjectiveScript : MonoBehaviour
     }
     void Update()
     {
+
+        QuestItems(Checker.isSetupComplete, ObjectiveRefractionLineBool, LineObjective, ObjectiveRefraction, CloseLeftEye, ObjectiveRefractionLine, CloseLeftEyeLine, OBJIndicator1);
+
+        QuestItems(Checker.IsRighteyeOpen, CloseLeftEyeLineBool, LineObjective2, CloseLeftEye, LSRight, CloseLeftEyeLine, LSRightLine, OBJIndicator2);
+
+        QuestItems(Checker.isRightSideLSComplete, LSRightLineBool, LineObjective3, LSRight, AstigAxisTMPright, LSRightLine, AstigAxisLineright, OBJIndicator3);
+
+        QuestItems(Checker.isRightSideAstigAxisComplete, isAstigAxisAnimationPlayedright, LineObjective4, AstigAxisTMPright, AstigMagTMPright, AstigAxisLineright, AstigMagLineright, OBJIndicator4);
+
+        QuestItems(Checker.isRightSideAstigMagComplete, isAstigMagAnimationPlayedright, LineObjective5, AstigMagTMPright, RightEyeFinal, AstigMagLineright, RightEyeFinalLine, OBJIndicator5);
+
+        QuestItems(Checker.isRightSideFinalComplete, RightEyeFinalLineBool, LineObjective6, RightEyeFinal, SwitchEyes, RightEyeFinalLine, SwitchEyesLine, OBJIndicator6);
+
+        QuestItems(Checker.IsLefteyeOpen, SwitchEyesLineBool, LineObjective7, SwitchEyes, LSLeft, SwitchEyesLine, LSLeftLine, OBJIndicator7);
+
+        QuestItems(Checker.isLeftSideLSComplete, LSLeftLineBool, LineObjective8, LSLeft, AstigAxisTMPleft, LSLeftLine, AstigAxisLineleft, OBJIndicator8);
+
+        QuestItems(Checker.isLeftSideAstigAxisComplete, isAstigAxisAnimationPlayedleft, LineObjective9, AstigAxisTMPleft, AstigMagTMPleft, AstigAxisLineleft, AstigMagLineleft, OBJIndicator9);
+
+        QuestItems(Checker.isLeftSideAstigMagComplete, isAstigMagAnimationPlayedleft, LineObjective10, AstigMagTMPleft , LeftEyeFinal, AstigMagLineleft , LeftEyeFinalLine, OBJIndicator10);
+
+        QuestItems(Checker.isLeftSideFinalComplete, LeftEyeFinalLineBool, LineObjective11, LeftEyeFinal, Congrats, LeftEyeFinalLine, CongratsLine, OBJIndicator11);
         /*
         if (Input.GetMouseButtonDown(0))
         {
             Pressed();
             Debug.Log("You Left Clicked");
         }
-        */
+
         
         QuestItems(Checker.isRightSideAstigAxisComplete, isAstigAxisAnimationPlayedleft, LineObjective, AstigAxisTMPleft, AstigMagTMPleft, AstigAxisLineleft, AstigMagLineleft, OBJIndicator1);
 
-        //QuestItems(Checker.isRightSideAstigMagComplete, isAstigMagAnimationPlayedleft, LineObjective2, AstigMagTMPleft, PlaceHolderTMP, AstigMagLineleft, PlaceHolderIMG, OBJIndicator2);
 
         QuestItems(Checker.isRightSideAstigMagComplete, isAstigMagAnimationPlayedleft, LineObjective2, AstigMagTMPleft, AstigAxisTMPright, AstigMagLineleft, AstigAxisLineright, OBJIndicator2);
 
         QuestItems(Checker.isLeftSideAstigAxisComplete, isAstigAxisAnimationPlayedright, LineObjective3, AstigAxisTMPright, AstigMagTMPright, AstigAxisLineright, AstigMagLineright, OBJIndicator3);
 
-        QuestItems(Checker.isLeftSideAstigMagComplete, isAstigMagAnimationPlayedright, LineObjective4, AstigMagTMPright, PlaceHolderTMP, AstigMagLineright, PlaceHolderIMG, OBJIndicator4);
+        //QuestItems(Checker.isLeftSideAstigMagComplete, isAstigMagAnimationPlayedright, LineObjective4, AstigMagTMPright, PlaceHolderTMP, AstigMagLineright, PlaceHolderIMG, OBJIndicator4);
 
-        /*
         if (Checker.isRightSideAstigAxisComplete && !isAstigAxisAnimationPlayedleft)
         {
             LineObjective.SetBool("Complete", true);
@@ -180,24 +260,58 @@ public class ObjectiveScript : MonoBehaviour
         switch (CurrentIndicator)
         {
             case 1:
-                isAstigAxisAnimationPlayedleft = true;
-            break; 
+                ObjectiveRefractionLineBool = true;
+                break; 
             
             case 2:
-                isAstigMagAnimationPlayedleft = true;
-            break;
+                CloseLeftEyeLineBool = true;
+                break;
 
             case 3:
-                isAstigAxisAnimationPlayedright = true;
-            break;
+                LSRightLineBool = true;
+                break;
 
             case 4:
-                isAstigMagAnimationPlayedright= true;
-            break;
+                isAstigAxisAnimationPlayedright = true;
+                break;
 
             case 5:
-                PlaceHolderBool = true;
-            break;
+                isAstigMagAnimationPlayedright = true;
+                break;
+
+            case 6:
+                RightEyeFinalLineBool = true;
+                break;
+
+            case 7:
+                SwitchEyesLineBool = true;
+                break;
+
+            case 8:
+                LSLeftLineBool = true;
+                break;
+
+            case 9:
+                isAstigAxisAnimationPlayedleft = true;
+                break;
+
+            case 10:
+                isAstigMagAnimationPlayedleft = true;
+                break;
+
+            case 11:
+                LeftEyeFinalLineBool = true; 
+                Submit.SetActive(true);
+                break;
+
+            case 12:
+                CongratsLineBool = true;
+
+                break;
+
+            default:
+                print("Ono :( its a logical error");
+                break;
 
         }
     }
