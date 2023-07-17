a230f520a2543bdbf102a787e07f6745d276a8f7
Jeronlow <65123862+Jeronlow@users.noreply.github.com>
2023-07-18 01:20:38 +0800

    New Checker & Input System for Keratometer #118

--- Assets/FYP Project/Scripts/Readings.cs
+++ Assets/FYP Project/Scripts/Readings.cs
@@ -1,17 +1,65 @@
+using Mono.Cecil;
+using TMPro;
+using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Readings : MonoBehaviour
 {
+    public int randomIndex;
+
     public GameObject object1;
+    private float[] TopAnswer1 = { 34.57f, 9.761f };
+    private float[] BottomAnswer1 = { 34.32f, 9.840f };
+
     public GameObject object2;
+    private float[] TopAnswer2 = { 45.41f, 7.431f };
+    private float[] BottomAnswer2 = { 34.57f, 7.731f };
+
     public GameObject object3;
+    private float[] TopAnswer3;
+    private float[] BottomAnswer3;
+
     public GameObject object4;
+    private float[] TopAnswer4;
+    private float[] BottomAnswer4;
+
     public GameObject object5;
+    private float[] TopAnswer5;
+    private float[] BottomAnswer5;
+
     public GameObject object6;
+    private float[] TopAnswer6;
+    private float[] BottomAnswer6;
+
     public GameObject object7;
+    private float[] TopAnswer7;
+    private float[] BottomAnswer7;
+
     public GameObject object8;
+    private float[] TopAnswer8;
+    private float[] BottomAnswer8;
+
     public GameObject object9;
+    private float[] TopAnswer9;
+    private float[] BottomAnswer9;
+
     public GameObject object10;
+    private float[] TopAnswer10;
+    private float[] BottomAnswer10;
+
+    public bool IslevelComplete = false;
+    public float Ans1;
+    public float Ans2;
+    public float Ans3;
+    public float Ans4;
+
+    public TMP_InputField inputFieldTop;
+    public TMP_InputField inputFieldBottom;
+
+
+    public bool IsTopCorrect;
+    public bool IsBottomCorrect;
 
     private void Start()
     {
@@ -28,14 +76,43 @@ public class Readings : MonoBehaviour
         object10.SetActive(false);
 
         // Randomly activate one of the game objects
-        int randomIndex = Random.Range(1, 11);
-        switch (randomIndex)
+        randomIndex = Random.Range(1, 3);
+        SelectGameObject(randomIndex, IslevelComplete);
+
+    }
+
+
+    private void Update()
+    {
+
+    }
+
+    private void SelectGameObject(int RandomInt, bool LevelState)
+    {
+        switch (RandomInt)
         {
             case 1:
                 object1.SetActive(true);
+                if (LevelState)
+                {
+
+                } else
+                {
+                    SetAnswers(object1, TopAnswer1, BottomAnswer1);
+                }
+
                 break;
             case 2:
                 object2.SetActive(true);
+                if (LevelState)
+                {
+
+                }
+                else
+                {
+                    SetAnswers(object2, TopAnswer2, BottomAnswer2);
+                }
+
                 break;
             case 3:
                 object3.SetActive(true);
@@ -63,6 +140,39 @@ public class Readings : MonoBehaviour
                 break;
         }
     }
+
+    private void SetAnswers(GameObject Readings, float[] TopAnsArray, float[] BottomAnsArray)
+    {
+        Ans1 = TopAnsArray[0];
+        Ans2 = TopAnsArray[1];
+        Ans3 = BottomAnsArray[0];
+        Ans4 = BottomAnsArray[1];
+    }
+
+    public void CheckAns()
+    {
+        string topInput = inputFieldTop.text;
+        string bottomInput = inputFieldBottom.text;
+
+        // Convert the input strings to floats
+        float topValue, bottomValue;
+        bool isTopValid = float.TryParse(topInput, out topValue);
+        bool isBottomValid = float.TryParse(bottomInput, out bottomValue);
+        print(topValue + 1.23);
+        print(bottomValue + 18);
+        if(topValue == Ans1)
+        {
+            print("yay");
+        }
+
+        if(bottomValue == Ans3)
+        {
+            print("yay2");
+        }
+    }
+
+
+
 }
 
 
